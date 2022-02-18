cafe40434c828afaf18151e57e2a4f32b7e8362e
Fran <fjimenezjob@gmail.com>
2022-02-18 15:27:46 +0100

    APP en desarollo

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, g, render_template, flash, url_for, redirect
+from flask_login import LoginManager
+import models
+import forms
+
+DEBUG = True
+PORT = 5000
+HOST = '0.0.0.0'
+
+app = Flask(__name__)
+app.secret_key = '49tfksadjfkpasbf--sdfsdf--//sdfsdf%sdfscsidjfSKMFSMDF'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view('login')
+
+@app.before_request
+def before_request():
+    """ Conecta a la base de datos antes de cada Request"""
+    
+    if not hasattr(g, 'db'):
+        g.db = models.DB
+        g.db.connect()
+
+@app.after_request
+def after_request(response):
+    """ Cerramos conexion a la BBDD """
+
+    g.db.close()
+    return response
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash('Has sido registrado !! ', 'success')
+        models.User.create_user(
+            username = form.username.data,
+            email = form.email.data,
+            password = form.password.data
+        )
+        return redirect(url_for('index'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        return models.User.get(models.User.id == user_id)
+    except models.DoesNotExist:
+        return None
+ 
+
+
+
+if __name__ == '__main__':
+    models.initialize()
+    models.User.create_user(
+        name = 'Fran',
+        email = 'fjimenezjob@gmail.com',
+        password = 'test',
+    )
+    app.run(debug = DEBUG, host = HOST, port =PORT)
---
+++ forms.py
@@ -0,0 +1,46 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import (DataRequired,ValidationError, Email, Regexp, Length, EqualTo)
+
+
+
+def name_exists(form, field):
+    if User.select().where(User.username == field.data).exists():
+        raise ValidationError('Ya existe un usuario con este nombre')
+
+def email_exists(form, field):
+        if User.select().where(User.email == field.data).exists():
+            raise ValidationError('Este email ya está registrado')
+
+
+
+class RefisterForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators = [
+            DataRequired(),
+            Regexp(
+                r'^[a-zA-Z0-9_]+$'
+            ),
+            name_exists
+        ])
+
+    email = StringField(
+        'Email',
+        validators = [
+            DataRequired(),
+            Email(),
+            email_exists
+        ])
+    
+    password = PasswordField(
+        'Password',
+        validators = [
+            DataRequired(),
+            Length(min=5),
+            EqualTo('password2', message='La contraseña debe de coincidir')
+        ])
+    
+    password2 = PasswordField(
+        'Confirm Password',
+        validators = [DataRequired()]
+    )
